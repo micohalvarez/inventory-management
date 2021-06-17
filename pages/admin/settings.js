a0fcf6c0f66f7eeb2d0e94edd7d2ee642514fe95
Raymund Micoh Alvarez <micohalvarez@yahoo.com>
2021-06-17 14:26:26 +0800

    Update settings.js

--- pages/admin/settings.js
+++ pages/admin/settings.js
@@ -1,13 +1,13 @@
-import React from 'react'
+// import React from 'react'
 
-export default function Settings () {
-    return (
-        <>
-            <div className="flex flex-wrap justify-center mt-24">
-                <div className="w-full lg:w-8/12 px-4"></div>
-            </div>
-        </>
-    )
-}
+// export default function Settings () {
+//     return (
+//         <>
+//             <div className="flex flex-wrap justify-center mt-24">
+//                 <div className="w-full lg:w-8/12 px-4"></div>
+//             </div>
+//         </>
+//     )
+// }
 
-Settings.layout = Admin
+// Settings.layout = Admin
